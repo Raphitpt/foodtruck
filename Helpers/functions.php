5278938fe78303ea3d4376f27ba7a4b1a641d252
Raphitpt <tiphonetraphi@gmail.com>
2023-10-17 09:43:35 +0200

    push des fichiers originaux

---
+++ Helpers/functions.php
@@ -0,0 +1,47 @@
+<?php
+/*
+    Fichier : /Helpers/functions.php
+ */
+
+/**
+ * Retourne le contenu HTML du bloc d'en tête d'une page.
+ * Deux CSS sont automatiquement intégré :
+ *   - pico.css
+ *   - custom.css
+ *
+ * @param string title le titre de la page.
+ * @return string
+ */
+function head(string $title = ''): string
+{
+    return  <<<HTML_HEAD
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+  <link rel="stylesheet" href="assets/css/custom.css">
+  <title>$title</title>
+</head>
+HTML_HEAD;
+}
+
+
+/**
+ * Retourne vrai si la méthode d'appel est GET.
+ */
+function isGetMethod(): bool
+{
+    return  ($_SERVER['REQUEST_METHOD'] === 'GET') ;
+}
+
+/**
+ * Retourne vrai si la méthode d'appel est POST.
+ */
+function isPostMethod(): bool
+{
+    return  ($_SERVER['REQUEST_METHOD'] === 'POST') ;
+}
---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+session_start();
+require 'bootstrap.php';
+
+if (isset($_POST['envoi'])) {
+    if (!empty($_POST['email']) && !empty($_POST['mdp'])) {
+        $email = htmlspecialchars($_POST['email']);
+        $mdp = $_POST['mdp'];
+
+        $recupUser = $dbh->prepare('SELECT * FROM users WHERE email = ? AND passwd = ?');
+        $recupUser->execute(array($email, $mdp));
+        if ($recupUser->rowCount() > 0) {
+            $_SESSION['user'] = $recupUser->fetch();
+            $_SESSION['email'] = $email;
+            header('Location: test.php');
+            exit();
+        } else {
+            echo "Votre mot de passe ou pseudo est incorrect";
+        }
+    } else {
+        echo "Veuillez compléter les champs.";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./assets/css/style.css" rel="stylesheet">
+    <title>Connexion</title>
+</head>
+<body>
+    <h1>Connexion</h1>
+    <form method="POST" action="">
+        <label for="username">Mail :</label>
+        <input type="text" name="email" >
+        <br>
+        <label for="password">Mot de passe :</label>
+        <input type="password" name="mdp" >
+        <br><br>
+        <input type="submit" name="envoi" value="Envoyer">
+    </form>
+</body>
+</html>
