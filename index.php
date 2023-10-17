762db5e551a5b1cab5e7208220ee96dcc20f5bc6
Raphitpt <tiphonetraphi@gmail.com>
2023-10-17 11:44:27 +0200

    fix index

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
 require './bootstrap.php';
-$plats= "SELECT * FROM plats";
+$plats = "SELECT * FROM plats";
 $plats = $dbh->query($plats);
 $plats = $plats->fetchAll();
 
 ?>
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -14,22 +15,26 @@ $plats = $plats->fetchAll();
     <link href="./assets/css/style.css" rel="stylesheet">
     <title>test</title>
 </head>
+
 <body>
+
     <h1>test de cvvo</h1>
     <button><a href="./login.php">Se connecter</a></button>
-    <button><p>Panier</p></button>
+    <button>
+        <p>Panier</p>
+    </button>
     <button>S'isncrire</button>
     <div>
         <?php
-        foreach($plats as $plat){
+        foreach ($plats as $plat) {
             echo "<div class='plat'>";
-            echo "<h2>".$plat['nom']."</h2>";
-            echo "<p>".$plat['composition']."</p>";
-            echo "<p>".$plat['prix']."€</p>";
+            echo "<h2>" . $plat['nom'] . "</h2>";
+            echo "<p>" . $plat['composition'] . "</p>";
+            echo "<p>" . $plat['prix'] . "€</p>";
             echo "</div>";
-
         }
         ?>
     </div>
 </body>
+
 </html>
