f314a7f762a3294ed69766d294b525c5e20ad480
Raphitpt <tiphonetraphi@gmail.com>
2023-10-17 10:47:54 +0200

    login

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
+<?php
+require './bootstrap.php';
+$plats= "SELECT * FROM plats";
+$plats = $dbh->query($plats);
+$plats = $plats->fetchAll();
 
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -11,5 +17,19 @@
 <body>
     <h1>test de cvvo</h1>
     <button><a href="./login.php">Se connecter</a></button>
+    <button><p>Panier</p></button>
+    <button>S'isncrire</button>
+    <div>
+        <?php
+        foreach($plats as $plat){
+            echo "<div class='plat'>";
+            echo "<h2>".$plat['nom']."</h2>";
+            echo "<p>".$plat['composition']."</p>";
+            echo "<p>".$plat['prix']."€</p>";
+            echo "</div>";
+
+        }
+        ?>
+    </div>
 </body>
 </html>
