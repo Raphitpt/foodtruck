3467e7f031077b4888baf3b13267bf68804ed563
Marius YVART <mariusyvt@gmail.com>
2023-12-04 16:46:44 +0100

    Confirmation ed MDP Fait

--- signin.php
+++ signin.php
@@ -7,12 +7,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   $prenom = $_POST['prenom'];
   $email = $_POST['email'];
   $password = $_POST['password'];
-  
+  $confirmPassword = $_POST['confirm_password'];
+
   // Validate the form data (you can add more validation rules here)
-  if (empty($nom) || empty($prenom) || empty($email) || empty($password)) {
+  if (empty($nom) || empty($prenom) || empty($email) || empty($password) || empty($confirmPassword)) {
     $error = 'Veuillez renseigner tous les champs';
+  } elseif ($password !== $confirmPassword) {
+    $error = 'Les mots de passe ne correspondent pas';
   } else {
-   $password = password_hash($password, PASSWORD_DEFAULT);
+    $password = password_hash($password, PASSWORD_DEFAULT);
     // Insert form data in the database
     $sql = "INSERT INTO users (nom, prenom, email, passwd) VALUES (:nom, :prenom, :email, :passwd)";
     $stmt = $dbh->prepare($sql);
@@ -30,24 +33,31 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
   <title>Connectez-vous</title>
 </head>
+
 <body>
   <h1>Register</h1>
   <?php if (isset($error)) { ?>
-    <p><?php echo $error; ?></p>
+    <p>
+      <?php echo $error; ?>
+    </p>
   <?php } ?>
   <form method="post">
     <label>Nom:</label>
-    <input type="text" name="nom"><br>    
+    <input type="text" name="nom"><br>
     <label>Prénom:</label>
     <input type="text" name="prenom"><br>
     <label>Email:</label>
     <input type="email" name="email"><br>
     <label>Mot de passe:</label>
     <input type="password" name="password"><br>
+    <label>Confirmation de Mot de passe</label>
+    <input type="password" name="confirm_password">
     <input type="submit" value="Register">
   </form>
 </body>
+
 </html>
