135b5d52e95da079a2019c33d7e0297af57e3c9e
Caparros <malo.caparros@etu.univ-poitiers.fr>
2024-01-11 12:26:42 +0100

    test

--- accueil.php
+++ accueil.php
@@ -7,6 +7,9 @@ $infos = $infosResult->fetch();
 echo head('Page d\'accueil');
 ?>
 <style>
+    body{
+        overflow-x:hidden;
+    }
     h1 {
         text-align: center;
     }
@@ -14,17 +17,53 @@ echo head('Page d\'accueil');
     img {
         width: 40%;
     }
-    .accueil{
-        height:calc(100vh - 4.5rem);
-        width:100vw;
-        display:flex;
-        flex-direction:column;
-        justify-content: space-around;
-        align-items: center;    
 
+    .accueil {
+        height:160vh;
+        width: 100vw;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        align-items: center;
     }
-    .accueil button{
-        width:20vw;
+
+    .accueil button {
+        width: 15vw;
+        padding:15px;
+    }
+
+    .accueil div:nth-child(3) {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .accueil div:nth-child(3) p {
+        height: 50%;
+        width: 30%;
+        text-align: center;
+    }
+
+    .accueil div:nth-child(4) {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .accueil div:nth-child(4) p {
+        height: 50%;
+        width: 40%;
+        padding:15px;
+        text-align: center;
+      
+    }
+    .accueil div:nth-child(4) img{
+        width:25vw;
+    }
+    footer{
+        height:10vh;
+        width:100vw;
+        background-color:black;
     }
 </style>
 
@@ -47,9 +86,28 @@ echo head('Page d\'accueil');
     <main>
         <section class="accueil">
             <h1>Bienvenue sur Fouée't Moi </h1>
-            <img src="./assets/img/Fichier 2.png"/>
             <button onclick="location.href = './index.php'">Aller commander</button>
+            <div>
+                <img src="./assets/img/Fichier 2.png" />
+                <p>
+                    La fouée est une petite boule de pain, cuite au four et fourrée encore chaude de rillettes,
+                    de grillon, de champignons, de mogettes ou de beurre, selon les régions. La fouée est issue du
+                    terroir gastronomique de l'ouest de la France (Touraine, Saumurois, Poitou, Charentes).
+                </p>
+            </div>
+            <div>
+                <p>
+                    Nous sommes un jeune couple charentais passionné par la création de fouée, tradition familiale
+                    depuis 50, nous nous engageons a vous préparer des fouées d'une grande qualité et à régaler
+                    vos papilles. On réalise des fouées sucrées et salés pour faire plaisir à tout le monde. Venez partager
+                    un repas avec nous, vous ne le regretterez pas.
+                </p>
+                <img src="./assets/img/vue-du-couple-3d.jpg" />
+
+            </div>
+
         </section>
     </main>
+    <footer></footer>
 
 </body>
