a55501aeeb7812a776ea2869e477c4aeadbab3e5
Marius YVART <mariusyvt@gmail.com>
2024-01-11 15:23:42 +0100

    zizi

--- index.php
+++ index.php
@@ -59,7 +59,10 @@ echo head('Accueil');
         </ul>
     </nav>
     <div class="infos_generale">
-        <p>Les horaires d'ouverture aujourd'hui sont : <?php echo $horaires['HeureOuverture'] ?>h - <?php echo $horaires['HeureFermeture'] ?>h</p>
+        <p>Les horaires d'ouverture aujourd'hui sont :
+            <?php echo $horaires['HeureOuverture'] ?>h -
+            <?php echo $horaires['HeureFermeture'] ?>h
+        </p>
     </div>
 
     <main class="main_commande">
@@ -67,12 +70,14 @@ echo head('Accueil');
 
         <section class="plats">
 
-        <div class="plats_title">
-                    <div class="plats_titles">
-                        <button type="button" class="button_fouee sel plats_title_salées" onclick="togglePlat('plats_salées')">fouées salées</button>
-                        <button type="button" class="button_fouee plats_title_sucrées" onclick="togglePlat('plats_sucrées')">fouées sucrées</button>
+            <div class="plats_title">
+                <div class="plats_titles">
+                    <button type="button" class="button_fouee sel plats_title_salées"
+                        onclick="togglePlat('plats_salées')">fouées salées</button>
+                    <button type="button" class="button_fouee plats_title_sucrées"
+                        onclick="togglePlat('plats_sucrées')">fouées sucrées</button>
 
-                    </div>
+                </div>
                 <div class="line"></div>
             </div>
             </div>
@@ -168,61 +173,6 @@ echo head('Accueil');
                 <!-- <a href="./order.php" class="order-button">Commander</a> -->
             </div>
         </section>
-        <div class="radio-inputs">
-            <label class="radio">
-                <input type="radio" name="radio" checked="">
-                <span class="name">HTML</span>
-            </label>
-            <label class="radio">
-                <input type="radio" name="radio">
-                <span class="name">React</span>
-            </label>
-
-            <label class="radio">
-                <input type="radio" name="radio">
-                <span class="name">Vue</span>
-            </label>
-        </div>
-        <style>
-            .radio-inputs {
-                position: relative;
-                display: flex;
-                flex-wrap: wrap;
-                border-radius: 0.5rem;
-                background-color: #EEE;
-                box-sizing: border-box;
-                box-shadow: 0 0 0px 1px rgba(0, 0, 0, 0.06);
-                padding: 0.25rem;
-                width: 300px;
-                font-size: 14px;
-            }
-
-            .radio-inputs .radio {
-                flex: 1 1 auto;
-                text-align: center;
-            }
-
-            .radio-inputs .radio input {
-                display: none;
-            }
-
-            .radio-inputs .radio .name {
-                display: flex;
-                cursor: pointer;
-                align-items: center;
-                justify-content: center;
-                border-radius: 0.5rem;
-                border: none;
-                padding: .5rem 0;
-                color: rgba(51, 65, 85, 1);
-                transition: all .15s ease-in-out;
-            }
-
-            .radio-inputs .radio input:checked+.name {
-                background-color: #fff;
-                font-weight: 600;
-            }
-        </style>
     </main>
 
     <script src="./assets/js/functions.js"></script>
