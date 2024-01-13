c1dfdd2d6061abdb0cad4afb3350e01f45a298be
Malo Caparros <malo.caparros@etu.univ-poitiers.fr>
2024-01-13 14:22:54 +0100

    tre

--- contact.php
+++ contact.php
@@ -46,6 +46,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 
 ?>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
 
 <body>
     <header>
@@ -95,10 +98,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     </header>
     <style>
         body {
-            font-family: Arial, sans-serif;
             background-color: #f4f4f4;
-            margin: 0;
-            padding: 0;
         }
 
         .container {
@@ -115,6 +115,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             margin-bottom: 8px;
         }
 
+
         input,
         textarea {
             width: 100%;
@@ -123,20 +124,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             border: 1px solid #ccc;
             border-radius: 4px;
             box-sizing: border-box;
-        }
-
-        button {
-            background-color: #4caf50;
-            color: #fff;
-            padding: 12px 20px;
-            border: none;
-            border-radius: 4px;
-            cursor: pointer;
-            font-size: 16px;
-        }
-
-        button:hover {
-            background-color: #45a049;
         }
     </style>
     <main>
@@ -159,7 +146,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     <label for="message">Message :</label>
                     <textarea id="message" name="message" rows="4" required></textarea>
 
-                    <button type="submit">Envoyer</button>
+                    <button type="submit" class="actions">Envoyer</button>
                 </form>
             </div>
         </section>
