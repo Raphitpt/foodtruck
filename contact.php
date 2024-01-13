5eff438104dcbe98cea6fdfc63fd6d4b2042b863
Malo Caparros <malo.caparros@etu.univ-poitiers.fr>
2024-01-13 14:56:54 +0100

    err

--- contact.php
+++ contact.php
@@ -138,7 +138,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             display: flex;
         }
 
-        img {
+        .form img {
             width: 15%;
             padding-right: 20px;
             transform: translateX(-10px);
