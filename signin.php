a64323ecab31e0629434e95efc1b600b5b9bedf9
Raphitpt <tiphonetraphi@gmail.com>
2024-01-15 10:49:18 +0100

    Refactor token generation and update email function

--- signin.php
+++ signin.php
@@ -24,8 +24,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $error = 'Cette adresse e-mail est déjà utilisée. Veuillez en choisir une autre.';
         } else {
             $password = password_hash($password, PASSWORD_DEFAULT);
-            $token = bin2hex(random_bytes(16));
-
+            function getRandomStringRandomInt($length = 16)
+            {
+                $stringSpace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+                $pieces = [];
+                $max = mb_strlen($stringSpace, '8bit') - 1;
+                for ($i = 0; $i < $length; ++$i) {
+                    $pieces[] = $stringSpace[random_int(0, $max)];
+                }
+                return implode('', $pieces);
+            }
+            $token = getRandomStringRandomInt();
             // Insert form data in the database
             $sql = "INSERT INTO users (nom, prenom, email, passwd, mailverif) VALUES (:nom, :prenom, :email, :passwd, :mailverif)";
             $stmt = $dbh->prepare($sql);
@@ -33,7 +42,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
             if ($stmt->rowCount() == 1) {
                 $success = 'Vous êtes maintenant enregistré !';
-                sendConfirmationMail($email, $token);
+                send_activation_email($email, $token);
                 $_SESSION['email'] = $email;
                 header('Location: verifMail.php');
             } else {
@@ -51,7 +60,7 @@ echo head('Inscription');
 
 
 <body>
-<header>
+    <header>
         <nav class="navfr">
             <ul class="nav_left">
                 <li class="nav_title"><img src="<?= htmlspecialchars($infos['url_logo']) ?>" alt="logo fouee">
@@ -124,33 +133,33 @@ echo head('Inscription');
             </nav>
         </div>
     </header>
-  <main>
-    <div class="btn-retour">
-      <a href="login.php" class="btn"><i class="fa-solid fa-arrow-left"></i></a>
-    </div>
+    <main>
+        <div class="btn-retour">
+            <a href="login.php" class="btn"><i class="fa-solid fa-arrow-left"></i></a>
+        </div>
 
-    <section class="form">
-      <h1>S'inscrire</h1>
-      <?php if (isset($error)) { ?>
-        <p>
-          <?php echo $error; ?>
-        </p>
-      <?php } ?>
-      <form method="post">
-        <label>Nom:</label>
-        <input type="text" name="nom"><br>
-        <label>Prénom:</label>
-        <input type="text" name="prenom"><br>
-        <label>Email:</label>
-        <input type="email" name="email"><br>
-        <label>Mot de passe:</label>
-        <input type="password" name="password"><br>
-        <label>Confirmation de Mot de passe</label>
-        <input type="password" name="confirm_password"><br>
-        <input type="submit" value="S'inscrire">
-      </form>
-  </main>
-  </section>
+        <section class="form">
+            <h1>S'inscrire</h1>
+            <?php if (isset($error)) { ?>
+                <p>
+                    <?php echo $error; ?>
+                </p>
+            <?php } ?>
+            <form method="post">
+                <label>Nom:</label>
+                <input type="text" name="nom"><br>
+                <label>Prénom:</label>
+                <input type="text" name="prenom"><br>
+                <label>Email:</label>
+                <input type="email" name="email"><br>
+                <label>Mot de passe:</label>
+                <input type="password" name="password"><br>
+                <label>Confirmation de Mot de passe</label>
+                <input type="password" name="confirm_password"><br>
+                <input type="submit" value="S'inscrire">
+            </form>
+    </main>
+    </section>
 
 
 </body>
